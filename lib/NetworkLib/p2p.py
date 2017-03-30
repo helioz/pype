19f2fe1af838c2ff44f70327dbe3ec2424b50ca2
NULL <soorejjones@gmail.com>
2017-03-30 12:44:04 +0530

    Added code to p2p library

--- lib/NetworkLib/p2p.py
+++ lib/NetworkLib/p2p.py
@@ -1,7 +1,43 @@
 ##Peer2peer library for establishing peer connection and send and recieve data
 
 
+class Peer:
+    def __init__(self, net_addr):
+        self.session_endpoints = net_addr
+        ## net_addr contains session endpoints and any other details to establish connection
+        self.tcpStream = 0 #Stream object
+
+    def sendUDP(self, data_bStream):
+        ## Function to send UDP packet to peer
+
+    def recieveUDP(self):
+        ## Function to recieve UDP packets
+        return data_bStream
+
+    def createTCPStream(self):
+        ##Creates a TCP Stream to self node
+
+    def sendTCP(self, data_bStream):
+        ##Sends data_bStream to node via tcpStream
+    def recieveTCP(self):
+        ##Recieves TCP packets from tcpStream
+        return data_bStream
+    def destroyTCP(self):
+        ##Closes TCP stream
+    
+    
 class p2p:
     def __init__(self):
-        self.ver = '0.0.1'
+        self.nodeList = []
 
+    def addNode(self, peer):
+        self.nodeList.append(peer)
+
+    def pushBroadcast(self, data_bStream, ctrlString):
+        for p in self.nodeList:
+            p.createTCPStream()
+            p.sendTCP(ctrlString)
+            p.sendTCP(data_bStream)
+            p.destroyTCP()
+        
+        
