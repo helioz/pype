0566a94ebfe96542f20f9688c9776dd99f76e475
Soorej Jones Pothoor <soorejjones@gmail.com>
2017-05-13 12:40:43 +0530

    Delete testAV.py

--- testAV.py
+++
@@ -1,9 +0,0 @@
-import lib.NetworkLib.p2p as p2p
-from lib.AVLib.AVWrapper import AVHandler
-import Resources._globals as G
-
-
-G.PORT_local = 6021
-peer = p2p.Peer("137.97.10.22+6821", p2p.SupportServer())
-
-AVHandler(peer).callAV()
